a4e94bc9cae0f1fb1988e2dd3549e8d19436165f
Nicolas Nueca <nicolasnueca@nicolass-air.lan>
2023-04-29 14:13:49 -0700

    fixed minZoom

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const myMap = {
 	buildMap() {
 		this.map = L.map('map', {
 		center: this.coordinates,
-		zoom: 11,
+		zoom: 10,
 		});
 		// add openstreetmap tiles
 		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution:
 			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-		minZoom: '15',
+		minZoom: '10',
 		}).addTo(this.map)
 		// create and add geolocation marker
 		const marker = L.marker(this.coordinates)
@@ -58,7 +58,7 @@ async function getFoursquare(business) {
 	let limit = 5
 	let lat = myMap.coordinates[0]
 	let lon = myMap.coordinates[1]
-	let response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
+	let response = await fetch(`https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
 	let data = await response.text()
 	let parsedData = JSON.parse(data)
 	let businesses = parsedData.results
@@ -90,5 +90,7 @@ window.onload = async () => {
 document.getElementById('submit').addEventListener('click', async (event) => {
 	event.preventDefault()
 	let business = document.getElementById('business').value
-	console.log(business)
+    let data = await getFoursquare(business)
+    myMap.businesses = processBusinesses(data)
+    myMap.addMarkers()
 })
